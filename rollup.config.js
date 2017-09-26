aff8b30b6f272cb38cde4660596e507a20f4a87e
Simon Kunz <simon@pixolith.de>
2017-09-26 21:31:16 +0200

    * refactored structure

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import buble from 'rollup-plugin-buble';
 const pkg = require('./package.json');
 
 export default {
-    input: './index.js',
+    input: './src/index.js',
     output: [
         { file: pkg.main, format: 'umd', name: 'vue-pouch-adapter' },
     ],
