f2c088df1dd3465caf7ba28f698521b4537f0f1a
assemblethis <46692058+assemblethis@users.noreply.github.com>
2019-04-23 14:14:26 -0600

    add component with selector that can return null

---
+++ tests/TodosDataFunctionWithSelector.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+<script>
+  export default {
+    data() {
+      // The simplest usage. queries all documents from the "todos" pouch database and assigns them to the "todos" vue property.
+      return { 
+          todos: null,//{/*empty selector*/},
+          age: 45,
+          maxAge: 40
+      }
+    }
+    
+    
+  }
+</script>
